2363f2a59c63e8aa21b0b1176436977cede98bbc
Kenneth Ho <kennethho@kenneths-mbp-4.myfiosgateway.com>
2024-12-15 16:54:24 -0500

    finished workshop page, added different scroll js library

--- public/workshop.js
+++ public/workshop.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({
+    duration: 1200,
+    once: false,
+    easing: "ease",
+  });
+});
+
 const host = window.location.origin;
 
 async function createAgent() {
@@ -39,6 +47,7 @@ async function createAgent() {
 }
 
 async function loadNewAgent() {
+  alert("Successfully Created an Agent!");
   // `${host}/agents`
   // "http://localhost:3000/agents"
   const response = await fetch(`${host}/agents`);
@@ -56,6 +65,7 @@ async function loadNewAgent() {
        </div>
    </div>
 `;
+
   new Swiper(".swiper-container", {
     slidesPerView: "auto",
     spaceBetween: 10,
@@ -100,7 +110,10 @@ const currentAgents = [
 function createAbilitySlides(data) {
   const newAgents = [];
   for (let i = 0; i < data.length; i++) {
-    if (!currentAgents.includes(data[i].agent_name)) {
+    if (
+      !currentAgents.includes(data[i].agent_name) &&
+      !exampleAgents.includes(data[i].agent_name)
+    ) {
       newAgents.push(data[i]);
     }
   }
@@ -139,4 +152,82 @@ function createAbilitySlides(data) {
     .join("");
 }
 
-window.onload = loadNewAgent();
+// window.onload = loadNewAgent();
+
+const exampleAgents = ["Sung Jin Woo", "Cha Hae In", "Choi Jong In"];
+
+async function loadExampleAgent() {
+  // `${host}/agents`
+  // "http://localhost:3000/agents"
+  const response = await fetch(`${host}/agents`);
+  const data = await response.json();
+  console.log("Data: ", data);
+
+  container_two = document.getElementById("exAgents");
+
+  // Show example agent details
+  container_two.innerHTML = `
+  <h2>Solo Leveling Inspired Agents!</h2>
+   <div class="swiper-container">
+       <div class="swiper-wrapper">
+           ${createExampleAgentSlides(data)}
+       </div>
+   </div>
+`;
+
+  new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: false,
+    },
+    freeMode: false,
+    simulateTouch: false,
+    allowTouchMove: false,
+  });
+}
+
+function createExampleAgentSlides(data) {
+  const newAgents = [];
+  for (let i = 0; i < data.length; i++) {
+    if (exampleAgents.includes(data[i].agent_name)) {
+      newAgents.push(data[i]);
+    }
+  }
+
+  return newAgents
+    .map((ability) => {
+      return `
+          <div class="swiper-slide">
+              <img src="${ability.agent_icon}" alt="${ability.agent_name}">
+              <h2>${ability.agent_name}</h2>
+              <p> <b>Agent Role:</b> ${ability.agent_role || "Unknown Role"}</p>
+              <p> <b>Agent Description:</b> ${
+                ability.agent_description || "No description available."
+              }</p>
+              <h3>Agent Abilities</h3>
+              <h4>${ability.ability_1_name || "Ability 1 Name Missing"}</h4>
+              <p>${
+                ability.ability_1_description || "Ability 1 Description Missing"
+              }</p>
+              <h4>${ability.ability_2_name || "Ability 2 Name Missing"}</h4>
+              <p>${
+                ability.ability_2_description || "Ability 2 Description Missing"
+              }</p>
+              <h4>${ability.grenade_name || "Grenade Name Missing"}</h4>
+              <p>${
+                ability.grenade_description || "Grenade Description Missing"
+              }</p>
+              <h4>${ability.ultimate_name || "Grenade Name Missing"}</h4>
+              <p>${
+                ability.ultimate_description || "Grenade Description Missing"
+              }</p>
+          </div>
+
+              `;
+    })
+    .join("");
+}
+
+window.onload = loadExampleAgent();
